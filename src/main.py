ae453bf5ed85c97442621d663bc0a0eea2410308
Mikołaj Nadra <m.nadra@proton.me>
2024-09-19 12:36:58 +0200

    feat: add option frame with button to edit tables

--- src/main.py
+++ src/main.py
@@ -10,16 +10,38 @@ class Window(ttk.Frame):
         self.parent.geometry("800x600")
 
         self.header = Frame(self.parent)
-        self.header.pack(side=TOP, fill=BOTH, padx=10, pady=10)
+        self.header.pack(anchor='ne', padx=50, pady=10)
         self.tableFrame = Frame(self.parent)
-        self.tableFrame.pack(side=TOP, fill=BOTH, padx=10)
+        self.tableFrame.pack(side=TOP, fill=BOTH, expand=True, padx=50, pady=10)
 
+        ttk.Label(self.header, text='Table:').pack(side=LEFT)
         self.combobox = ttk.Combobox(self.header, values=['Student', 'Subject'])
-        self.combobox.pack(side=RIGHT)
+        self.combobox.pack(side=RIGHT, fill=BOTH)
         self.combobox.bind("<<ComboboxSelected>>", self.displayTable)
-        ttk.Label(self.header, text='Table:').pack(side=RIGHT)
 
+        self.tree = ttk.Treeview(self.tableFrame, show='headings')
+        self.tree.pack(expand=True, fill=BOTH)
 
+        self.optionsFrame = ttk.LabelFrame(self.parent, text='Options')
+        self.optionsFrame.pack(side=BOTTOM, padx=50, pady=50, ipady=10)
+
+        self.addButton = ttk.Button(self.optionsFrame, text='Add', command=self.add)
+        self.editButton = ttk.Button(self.optionsFrame, text='Edit', command=self.edit)
+        self.deleteButton = ttk.Button(self.optionsFrame, text='Delete', command=self.delete)
+
+        self.addButton.pack(side=LEFT, padx=10)    
+        self.editButton.pack(side=LEFT, padx=10)
+        self.deleteButton.pack(side=LEFT, padx=10)
+
+    def edit(self):
+        pass
+
+    def delete(self):
+        pass
+    
+    def add(self):
+        pass
+    
     def displayTable(self, event) -> None:
         table = self.combobox.get()
         columns = []
@@ -48,7 +70,7 @@ class Window(ttk.Frame):
         for row in table:
             self.tree.insert('', 'end', values=[column for column in row])
     
-        self.tree.pack()
+        self.tree.pack(expand=True, fill=BOTH)
 
 if __name__ == "__main__":
     root = Tk()
