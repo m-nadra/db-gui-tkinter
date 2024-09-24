c3059d1780a57e4ffa8249c678f9e558bd07b490
Mikołaj Nadra <m.nadra@proton.me>
2024-09-24 14:47:11 +0200

    move refreshTable method to TableContent class

--- src/main.py
+++ src/main.py
@@ -12,14 +12,15 @@ class MainWindow(Tk):
         self.header = Header(self)
         self.table = Table(self)
         self.tableContent = TableContent(self.header, self.table)
-        self.recordManager = RecordManager(self.header, self.table)
-        self.options = Options(self, self.tableContent, self.recordManager)
+        self.recordManager = RecordManager(self.header, self.tableContent)
+        self.options = Options(self, self.recordManager)
 
 
 class Header(Frame):
     def __init__(self, parent):
         super().__init__(parent)
         self.pack(anchor='ne', padx=50, pady=10)
+
         ttk.Label(self, text='Table:').pack(side=LEFT)
         self.combobox = ttk.Combobox(self, values=queries.getTablesNames())
         self.combobox.pack(side=RIGHT, fill=BOTH)
@@ -30,32 +31,16 @@ class Table(Frame):
     def __init__(self, parent):
         super().__init__(parent)
         self.pack(side=TOP, fill=BOTH, expand=True, padx=50, pady=10)
+
         self.tree = ttk.Treeview(self, show='headings')
-        self.tree.pack(expand=True, fill=BOTH)
-
-    def refreshTable(self, columns, records):
-        try:
-            self.tree.destroy()
-        except AttributeError:
-            pass
-
-        self.tree = ttk.Treeview(self, columns=columns, show='headings')
-
-        for column in columns:
-            self.tree.heading(column, text=column)
-
-        for row in records:
-            self.tree.insert('', 'end', values=[column for column in row])
-
         self.tree.pack(expand=True, fill=BOTH)
 
 
 class Options(ttk.LabelFrame):
-    def __init__(self, parent, tableContent, recordManager):
+    def __init__(self, parent, recordManager):
         super().__init__(parent, text='Options')
-        self.tableContent = tableContent
-        self.recordManager = recordManager
         self.pack(side=BOTTOM, padx=50, pady=50, ipady=10)
+        self.recordManager = recordManager
 
         ttk.Button(self, text='Add', command=self.recordManager.addRecord).pack(side=LEFT, padx=10)
         ttk.Button(self, text='Edit', command=self.recordManager.editRecord).pack(side=LEFT, padx=10)
--- src/table.py
+++ src/table.py
@@ -5,7 +5,7 @@ import queries
 class TableContent:
     def __init__(self, header, table):
         self.header = header
-        self.table = table
+        self.tree = table.tree
 
     def updateTable(self):
         tableName = self.header.combobox.get()
@@ -14,7 +14,20 @@ class TableContent:
         records = tableClass.get()
         columns = tableClass.getColumnNames()
 
-        self.table.refreshTable(columns, records)
+        self.refreshTable(columns, records)
+
+    def refreshTable(self, columns, records):
+        for col in self.tree.get_children():
+            self.tree.delete(col)
+
+        self.tree["columns"] = columns
+        
+        for column in columns:
+            self.tree.heading(column, text=column)
+            self.tree.column(column, width=100)
+
+        for row in records:
+            self.tree.insert('', 'end', values=[column for column in row])
 
 
 class RecordManager:
@@ -25,7 +38,7 @@ class RecordManager:
     def addRecord(self):
         addWindow = AddWindow(self.header.combobox.get())
         self.header.wait_window(addWindow)
-        self.updateTable()
+        self.table.updateTable()
         
     def deleteRecord(self):
         try:
@@ -43,17 +56,7 @@ class RecordManager:
         if tableClass:
             tableClass.deleteRecord(recordId)
 
-        self.updateTable()
-
-    def updateTable(self):
-        tableName = self.header.combobox.get()
-        tableClass = getattr(queries, tableName, None)
-        
-        records = tableClass.get()
-        columns = tableClass.getColumnNames()
-
-        self.table.refreshTable(columns, records)
+        self.table.updateTable()
 
     def editRecord(self):
         pass
-
