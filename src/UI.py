c29b2b07e98869aa75ccab9baee590bbc3bbabf1
Mikołaj Nadra <m.nadra@proton.me>
2024-09-26 18:47:01 +0200

    doc: UI.py

--- src/UI.py
+++ src/UI.py
@@ -1,22 +1,29 @@
+"""This module contains the classes that define the layout of the main window."""
+
 from tkinter import Frame, LEFT, TOP, RIGHT, BOTTOM, BOTH
 from tkinter import ttk
 
 
 class MainWindowLayout:
-    def __init__(self, root):
-        root.title("School Manager")
-        root.geometry("800x600")
-        self.header = Header(root)
-        self.table = Table(root)
-        self.options = Options(root)
+    """Creates frames and widgets for the main window."""
 
-    def getComponents(self):
+    def __init__(self, parentWindow):
+        parentWindow.title("School Manager")
+        parentWindow.geometry("800x600")
+        self.header = Header(parentWindow)
+        self.table = Table(parentWindow)
+        self.options = Options(parentWindow)
+
+    def getComponents(self) -> tuple:
+        """Returns the components of the main window."""
         return self.header, self.table, self.options
 
 
 class Header(Frame):
-    def __init__(self, parent):
-        super().__init__(parent)
+    """Header frame with a combobox to select the table."""
+
+    def __init__(self, parentWindow):
+        super().__init__(parentWindow)
         self.pack(anchor='ne', padx=50, pady=10)
 
         ttk.Label(self, text='Table:').pack(side=LEFT)
@@ -25,22 +32,28 @@ class Header(Frame):
 
 
 class Table(Frame):
-    def __init__(self, parent):
-        super().__init__(parent)
+    """Table frame with a table to display the records."""
+
+    def __init__(self, parentWindow):
+        super().__init__(parentWindow)
         self.pack(side=TOP, fill=BOTH, expand=True, padx=50, pady=10)
 
-        self.tree = ttk.Treeview(self, show='headings')
-        self.tree.pack(expand=True, fill=BOTH)
+        self.table = ttk.Treeview(self, show='headings')
+        self.table.pack(expand=True, fill=BOTH)
 
 
 class Options(ttk.LabelFrame):
-    def __init__(self, parent):
-        super().__init__(parent, text='Options')
+    """Options frame with buttons to add, edit, and delete records."""
+
+    def __init__(self, parentWindow):
+        super().__init__(parentWindow, text='Options')
         self.pack(side=BOTTOM, padx=50, pady=50, ipady=10)
 
         self.addButton = ttk.Button(self, text='Add')
         self.addButton.pack(side=LEFT, padx=10)
+
         self.editButton = ttk.Button(self, text='Edit')
         self.editButton.pack(side=LEFT, padx=10)
+
         self.deleteButton = ttk.Button(self, text='Delete')
         self.deleteButton.pack(side=LEFT, padx=10)
