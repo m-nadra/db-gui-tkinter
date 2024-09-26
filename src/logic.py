1e887621ea1de892ac45efe75c1d7ba20813908a
Mikołaj Nadra <m.nadra@proton.me>
2024-09-26 19:14:40 +0200

    doc: create docstrings for logic.py

--- src/logic.py
+++ src/logic.py
@@ -1,16 +1,22 @@
-import queries
+"""This module contains the classes to manipulate data in widgets."""
+
 from windows import AddWindow
 from tkinter import messagebox
+import queries
 
 
 class MainWindowLogic:
-    def __init__(self, header, table):
-        self.tableFrame = table
-        self.tableContent = TableContent(table.tree)
-        self.tableSelector = TableSelector(header.combobox, self.tableContent)
+    """Create the logic for the main window."""
 
-    def addRecord(self):
-        tableName = self.tableContent.tableName
+    def __init__(self, headerFrame, tableFrame):
+        self.tableFrame = tableFrame
+        self.tableContent = TableContent(tableFrame.table)
+        self.tableSelector = TableSelector(
+            headerFrame.combobox, self.tableContent)
+
+    def addRecord(self) -> None:
+        """Create a window with form to add a record. After adding the record, update the table widget."""
+        tableName = self.tableContent.getTableName()
         addWindow = AddWindow(tableName)
         self.tableFrame.wait_window(addWindow)
         self.tableContent.updateTable(tableName)
@@ -18,56 +24,73 @@ class MainWindowLogic:
     def editRecord(self):
         pass
 
-    def deleteRecord(self):
+    def deleteRecord(self) -> None:
+        """Delete the selected record. After deleting the record, update the table widget."""
         try:
-            recordId = self.tableContent.tree.item(
-                self.tableContent.tree.selection())['values'][0]
+            recordId = self.tableContent.getSelectedRecord()
         except IndexError:
             messagebox.showerror("Error", "No record selected")
             return
 
-        result = messagebox.askquestion(
+        userConfirmation = messagebox.askquestion(
             "Confirm deletion", "Do you want to remove the record?")
-        if result == 'no':
+        if userConfirmation == 'no':
             return
 
-        tableName = self.tableContent.tableName
+        tableName = self.tableContent.getTableName()
         tableClass = getattr(queries, tableName, None)
-        if tableClass:
-            tableClass.deleteRecord(recordId)
-
+        tableClass.deleteRecord(recordId)
         self.tableContent.updateTable(tableName)
 
 
 class TableContent:
-    def __init__(self, tree):
-        self.tree = tree
-        self.tableName = ""
-        self.columns = []
-        self.rows = []
+    """Manage the content of the table widget."""
 
-    def updateTable(self, tableName):
+    def __init__(self, tableWidget):
+        self.tableWidget = tableWidget
+        self.tableName: str
+        self.rows: list
+        self.columnNames: list
+
+    def updateTable(self, tableName: str) -> None:
+        """Update object attributes and refresh the table widget."""
         self.tableName = tableName
         tableClass = getattr(queries, tableName, None)
-        self.records = tableClass.get()
-        self.columns = tableClass.getColumnNames()
+        self.rows = tableClass.get()
+        self.columnNames = tableClass.getColumnNames()
         self.refreshTable()
 
-    def refreshTable(self):
-        for col in self.tree.get_children():
-            self.tree.delete(col)
+    def refreshTable(self) -> None:
+        """Delete all the records in the table widget and insert the new records."""
+        self.clearTableWidget()
 
-        self.tree["columns"] = self.columns
+        self.tableWidget["columns"] = self.columnNames
 
-        for column in self.columns:
-            self.tree.heading(column, text=column)
-            self.tree.column(column, width=100)
+        for column in self.columnNames:
+            self.tableWidget.heading(column, text=column)
+            self.tableWidget.column(column, width=100)
 
-        for row in self.records:
-            self.tree.insert('', 'end', values=[column for column in row])
+        for row in self.rows:
+            self.tableWidget.insert(
+                '', 'end', values=[column for column in row])
+
+    def getTableName(self) -> str:
+        """Return the name of the dispaled table."""
+        return self.tableName
+
+    def getSelectedRecord(self) -> int:
+        """Return the id of the selected record"""
+        return self.tableWidget.item(self.tableWidget.selection())['values'][0]
+
+    def clearTableWidget(self) -> None:
+        """Remove all the records in the table widget"""
+        for col in self.tableWidget.get_children():
+            self.tableWidget.delete(col)
 
 
 class TableSelector:
+    """Manage the table selection combobox."""
+
     def __init__(self, combobox, tableContent):
         self.combobox = combobox
         self.combobox['values'] = queries.getTablesNames()
